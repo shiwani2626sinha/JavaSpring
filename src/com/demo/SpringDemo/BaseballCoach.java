1d0d102246c63561eb3e3f49f3ee81afe03ed3f4
shiwani2626sinha <shiwani2626@gmail.com>
2021-09-20 10:20:19 +0530

    setter injection

--- src/com/demo/SpringDemo/BaseballCoach.java
+++ src/com/demo/SpringDemo/BaseballCoach.java
@@ -2,9 +2,20 @@ package com.demo.SpringDemo;
 
 public class BaseballCoach implements Coach {
 	
+	Fortune fortune;
+	
+	public void setFortune(Fortune fortune) {
+		this.fortune = fortune;
+	}
+
+
 	@Override
 	public void getDailyWorkout() {
 		System.out.println("baseball 20 mins");
 	}
 
+	@Override
+	public void getDailyFortune() {
+		fortune.getFortune();
+	}
 }
