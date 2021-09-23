1004618d5aa7a2990fb0b12efc257d59408579fd
shiwani2626sinha <shiwani2626@gmail.com>
2021-09-24 00:16:19 +0530

    added prototyping

--- src/com/demo/SpringDemo/HelloSpring.java
+++ src/com/demo/SpringDemo/HelloSpring.java
@@ -15,6 +15,13 @@ public class HelloSpring {
 		//id and provide the interface on which spring class depends
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
+		Coach AlphaCoach = context.getBean("myCoach", Coach.class); 
+		
+		//default scope where same bean is created, singleton bean. 
+		System.out.println(theCoach == AlphaCoach);
+		
+		
+		
 		//use bean.function
 		theCoach.getDailyWorkout();
 		//DI FOR EXTERNAL CLASS
